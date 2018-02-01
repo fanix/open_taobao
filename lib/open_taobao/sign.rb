95d9d3d4091d32d74524e81ce9d1776ccd9edbfd
zlei1 <zhenglei123a@gmail.com>
2018-02-01 17:40:37 +0800

    sign argument error

--- lib/open_taobao/sign.rb
+++ lib/open_taobao/sign.rb
@@ -1,11 +1,11 @@
 module OpenTaobao
   class Sign
     class << self
-      def md5
+      def md5(string)
         Digest::MD5.hexdigest(string)
       end
 
-      def hmac
+      def hmac(string)
         HMAC::MD5.new(string).digest
       end
     end
