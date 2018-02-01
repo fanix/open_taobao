222fd01c2e1d57065f444b0ff23247332aa7f581
zlei1 <zhenglei123a@gmail.com>
2018-02-01 16:52:56 +0800

    test helper add app_key app_secret

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "open_taobao"
 
 require "minitest/autorun"
+
+OpenTaobao.app_key = '1000000000000000'
+OpenTaobao.app_secret = '10000000000000000000000000000000'
