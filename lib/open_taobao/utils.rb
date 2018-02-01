314365a1bf8f9553fac0eb7ea45552670b398c76
zlei1 <zhenglei123a@gmail.com>
2018-02-01 12:28:58 +0800

    utils add to_query

--- lib/open_taobao/utils.rb
+++ lib/open_taobao/utils.rb
@@ -1,8 +1,14 @@
+require "uri"
+
 module OpenTaobao
   class Utils
     class << self
-      def to_signed hash app_secret
+      def to_sign hash app_secret
         "#{app_secret}"<<hash.sort.flatten.join<<"#{app_secret}"
+      end
+
+      def to_query hash
+        hash.map{ |k, v| [k, URI.encode_www_form_component(v)].join("=") }.join("&")
       end
     end
   end
