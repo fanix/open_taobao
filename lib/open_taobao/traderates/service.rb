3daf7c0d6da2366804c6a54f6c73c0f4ec2145ea
zlei1 <zhenglei123a@gmail.com>
2018-04-12 11:59:08 +0800

    syntax error

--- lib/open_taobao/traderates/service.rb
+++ lib/open_taobao/traderates/service.rb
@@ -13,23 +13,23 @@ module OpenTaobao
         sign_method = (options[:sign_method] || :md5).to_s
         return_format = (options[:return_format] || :json).to_s
 
-        params = OpenTaobao::Utils.base_params('taobao.traderates.get', app_key, sign_method, return_format).merge({
+        real_params = OpenTaobao::Utils.base_params('taobao.traderates.get', app_key, sign_method, return_format).merge({
                  "session"    => params["session"],
                  "fields"     => params["fields"],
                  "rate_type"  => params["rate_type"],
                  "role"       => params["role"]
           })
 
-        params["result"] => params["result"] if params["result"].present?
-        params["page_no"] => params["page_no"] if params["page_no"].present?
-        params["page_size"] => params["page_size"] if params["page_size"].present?
-        params["start_date"] => params["start_date"] if params["start_date"].present?
-        params["end_date"] => params["end_date"] if params["end_date"].present?
-        params["tid"] => params["tid"] if params["tid"].present?
-        params["num_iid"] => params["num_iid"] if params["num_iid"].present?
-        params["use_has_next"] => params["use_has_next"] if params["use_has_next"].present?
+        real_params["result"] => params["result"] if params["result"].present?
+        real_params["page_no"] => params["page_no"] if params["page_no"].present?
+        real_params["page_size"] => params["page_size"] if params["page_size"].present?
+        real_params["start_date"] => params["start_date"] if params["start_date"].present?
+        real_params["end_date"] => params["end_date"] if params["end_date"].present?
+        real_params["tid"] => params["tid"] if params["tid"].present?
+        real_params["num_iid"] => params["num_iid"] if params["num_iid"].present?
+        real_params["use_has_next"] => params["use_has_next"] if params["use_has_next"].present?
 
-        signed_params = params.merge("sign" => OpenTaobao::Utils.get_sign(params, app_secret, sign_method))
+        signed_params = params.merge("sign" => OpenTaobao::Utils.get_sign(real_params, app_secret, sign_method))
 
         OpenTaobao::Utils.url_with_params signed_params, OpenTaobao.gateway_url
       end
