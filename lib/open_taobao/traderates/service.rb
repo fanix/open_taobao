bbd8b796253b559cd566ac6b4dce5f25ed9b3e63
zlei1 <zhenglei123a@gmail.com>
2018-04-12 11:39:26 +0800

    taobao_traderates_get 接口调整

--- lib/open_taobao/traderates/service.rb
+++ lib/open_taobao/traderates/service.rb
@@ -2,7 +2,7 @@ module OpenTaobao
   module Traderates
     module Service
       #搜索评价信息
-      TAOBAO_TRADERATES_GET = %w( session fields rate_type role page_no page_size )
+      TAOBAO_TRADERATES_GET = %w( session fields rate_type role )
       def self.taobao_traderates_get(params, options = {})
         params = OpenTaobao::Utils.stringify_keys(params)
         OpenTaobao::Validation.check_required_params(params, TAOBAO_TRADERATES_GET)
@@ -17,10 +17,17 @@ module OpenTaobao
                  "session"    => params["session"],
                  "fields"     => params["fields"],
                  "rate_type"  => params["rate_type"],
-                 "role"       => params["role"],
-                 "page_no"    => params["page_no"],
-                 "page_size"  => params["page_size"]
+                 "role"       => params["role"]
           })
+
+        params["result"] => params["result"] if params["result"].present?
+        params["page_no"] => params["page_no"] if params["page_no"].present?
+        params["page_size"] => params["page_size"] if params["page_size"].present?
+        params["start_date"] => params["start_date"] if params["start_date"].present?
+        params["end_date"] => params["end_date"] if params["end_date"].present?
+        params["tid"] => params["tid"] if params["tid"].present?
+        params["num_iid"] => params["num_iid"] if params["num_iid"].present?
+        params["use_has_next"] => params["use_has_next"] if params["use_has_next"].present?
 
         signed_params = params.merge("sign" => OpenTaobao::Utils.get_sign(params, app_secret, sign_method))
 
